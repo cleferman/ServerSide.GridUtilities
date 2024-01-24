0a80262ccd5c235e5fcd3ad22bb74b0b32714802
Cosmin Leferman <cosmin.leferman@accesa.eu>
2024-01-24 23:48:36 +0200

    added path filters

--- ServerSide.GridUtilities/Extensions/EnumExtensions.cs
+++ ServerSide.GridUtilities/Extensions/EnumExtensions.cs
@@ -5,7 +5,7 @@ namespace ServerSide.GridUtilities.Extensions;
 
 public static class EnumExtensions
 {
-    public static string GetDescription(this Enum value)
+    public static string? GetDescription(this Enum value)
     {
         Type type = value.GetType();
         string name = Enum.GetName(type, value);
@@ -14,10 +14,7 @@ public static class EnumExtensions
             FieldInfo field = type.GetField(name);
             if (field != null)
             {
-                DescriptionAttribute attr =
-                       Attribute.GetCustomAttribute(field,
-                         typeof(DescriptionAttribute)) as DescriptionAttribute;
-                if (attr != null)
+                if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attr)
                 {
                     return attr.Description;
                 }
